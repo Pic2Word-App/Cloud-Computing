bfb6052c95a3813e75afe11db305ffac56088d80
Rahmad Setiya Budi <222011623@stis.ac.id>
2024-06-15 19:20:39 +0700

    add function validate password

--- database/crud.py
+++ database/crud.py
@@ -32,6 +32,9 @@ def get_user_by_id(db: Session, user_id: int):
         )
 
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
 def get_password_hash(password):
     return pwd_context.hash(password)
 
